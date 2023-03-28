f4a1b2358d3d785ee39b82a7011e28251cd9ed7f
Enosh <125718166+Enosh3200@users.noreply.github.com>
2023-03-29 00:27:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #Chatbot with Python
-import random
-from datetime import datetime
-#saytime = time.ctime()
-DateandTime = datetime.now()
+
+
 
 chatBot = {
     "hai":"Hello",
@@ -20,19 +18,7 @@ chatBot = {
 }
 
 while True:
-    def random_string():
-        random_list = [
-            "Please try writing something more descriptive.",
-            "Oh! It appears you wrote something I don't understand yet",
-            "Do you mind trying to rephrase that?",
-            "I'm terribly sorry, I didn't quite catch that.",
-            "I can't answer that yet, please try asking something else."
-        ]
-
-        list_count = len(random_list)
-        random_item = random.randrange(list_count)
-
-        return random_list[random_item]
+   
 
 
     Human = input("You: ")
